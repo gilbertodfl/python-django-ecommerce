7f5e2e145764f9370bf508690f27065b3b6ff736
gilbertodfl <gilbertofl@tjdft.jus.br>
2026-03-12 18:23:28 -0300

    fix: filtro estava usando Produtos e não produtos(lista)

--- loja/utils.py
+++ loja/utils.py
@@ -1,10 +1,8 @@
 def filtrar_produtos(produtos, filtro):
-    from .models import Produto, Categoria
     if filtro:    
         if  "-" in filtro:
             categoria_slug, tipo_slug = filtro.split("-")
             produtos = produtos.filter(tipo__slug=tipo_slug, categoria__slug=categoria_slug,ativo=True)
-            produtos = Produto.objects.filter(tipo__slug=tipo_slug,ativo=True)
         else:
             produtos = produtos.filter(categoria__nome=filtro,ativo=True)
     return produtos
