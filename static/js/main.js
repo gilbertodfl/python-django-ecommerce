f2c585d0d7dff1732c999dc50f1ccfd870836d27
gilbertodfl <gilbertofl@tjdft.jus.br>
2026-03-24 17:05:54 -0300

    refatorando banner e filtro

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 var url = new URL(document.URL);
-var itens= document.getElementsByClassName("item-ordernar");
+var itens= document.getElementsByClassName("produtos__select");
 for( i = 0; i < itens.length; i++){
     url.hash = ""; // remove o # do final
-    url.searchParams.set("ordem",itens[i].name);
-    itens[i].href=url.href;
+    url.searchParams.set("ordem",itens[i].value);
+    itens[i].value=url.href;
 }
